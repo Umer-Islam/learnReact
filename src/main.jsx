1c609686fc2543c108faa7d67c72642d06e72d6f
Umer-Islam <139012668+Umer-Islam@users.noreply.github.com>
2024-01-12 19:44:07 +0500

    useState small functions

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,12 @@ import "./index.css";
 import'./App.css'
 import ClickEvent from "./components/ClickEvent.jsx";
 import List  from "./components/List.jsx";
+import State from "./components/State.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <App /> */}
-<ClickEvent />
+{/* <ClickEvent /> */}
+<State />
     {/* <List /> */}
   </React.StrictMode>
 );

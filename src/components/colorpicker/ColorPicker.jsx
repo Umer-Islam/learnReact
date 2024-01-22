476168cb69909b2b5d66e5fc5778a4eb3bcd823e
Umer-Islam <139012668+Umer-Islam@users.noreply.github.com>
2024-01-22 07:52:15 +0500

    formating using prettier

--- src/components/colorpicker/ColorPicker.jsx
+++ src/components/colorpicker/ColorPicker.jsx
@@ -1,25 +1,24 @@
-import { useState } from "react"
+import { useState } from "react";
 
-useState
+useState;
 export default function ColorPicker() {
-const[color,setColor]= useState('#d33636
+  const [color, setColor] = useState("#d33636");
+  function handleColorChange(e) {
+    setColor(e.target.value);
+  }
 
-')
-function handleColorChange (e){
-    setColor(e.target.value)
-
-}   
-
-return (
+  return (
     <div>
-<div className="color-picker">
-    <div className="selector">
-        <h1 className="first-heading">color picker</h1>
-        <p className="color-display" style={{backgroundColor:color}} >{color} </p>
-        <label htmlFor="input"> select a color </label>
- <input type="color"value={color} onChange={handleColorChange} />
+      <div className="color-picker">
+        <div className="selector">
+          <h1 className="first-heading">color picker</h1>
+          <p className="color-display" style={{ backgroundColor: color }}>
+            {color}{" "}
+          </p>
+          <label htmlFor="input"> select a color </label>
+          <input type="color" value={color} onChange={handleColorChange} />
+        </div>
+      </div>
     </div>
-</div>
-    </div>
-  )
+  );
 }
